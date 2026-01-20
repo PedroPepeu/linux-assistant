1b17ce179bf2691e59777881a5d05fcdc665b492
PedroPepeu <pedrosteamgtav@gmail.com>
2026-01-20 02:06:35 -0300

    Wake Word
    Created an wake_word function that will start the whole program
    when it detects the user saying the word selected "bob". And returns
    '>>> HELLO <<<', when correctly detected.
    
    Signed-off-by: PedroPepeu <pedrosteamgtav@gmail.com>

---
+++ src/wake_word.py
@@ -0,0 +1,38 @@
+import speech_recognition as sr
+import os
+
+# This hides the ALSA warnings programmatically (alternative to the terminal command)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+def start_wake_word():
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+    WAKE_WORD = "bob"
+
+    with microphone as source:
+        print("Calibrating mic for background noise...")
+        recognizer.adjust_for_ambient_noise(source, duration=1)
+    
+    print(f"Ready! Listening for: '{WAKE_WORD}'")
+
+    while True:
+        try:
+            with microphone as source:
+                # listen() will wait until it hears a phrase
+                audio = recognizer.listen(source, timeout=None, phrase_time_limit=3)
+
+            text = recognizer.recognize_google(audio).lower()
+            print(f"Found: {text}")
+
+            if WAKE_WORD in text:
+                print(">>> HELLO <<<")
+
+        except sr.UnknownValueError:
+            continue 
+        except sr.RequestError:
+            print("Check internet.")
+        except Exception as e:
+            continue
+
+if __name__ == "__main__":
+    start_wake_word()
